80e031f283ea09d302d13ca623f3d2645247e888
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-03-23 00:53:09 +0900

    [Platinum V] Title: Corrupt Election, Time: 4 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Platinum/13135. Corrupt Election/Corrupt Election.cc
+++ 백준/Platinum/13135. Corrupt Election/Corrupt Election.cc
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define int long long
+#define ll long long
 #define fi first
 #define se second
 #define all(a) (a).begin(), (a).end()
@@ -23,42 +24,28 @@ void compress() {
         int a = arr[i].fi, b = arr[i].se;
         int y = lower_bound(all(vy), a + b) - vy.begin(); 
         int x = lower_bound(all(vx), max(a, b)) - vx.begin();
-        //cout << a + b << " " << max(a, b) << " " << y << " " << x << "\n";
         vp[x].push_back({ y, i });
     }    
-
-    // for (int i : vx) cout << i << " ";
-    // cout << "\n";
-    // for (int i : vy) cout << i << " ";
-    // cout << "\n";
 }
-
-
 
 void count(int yi, int xi) {
     int dy = vy[yi] - vy[yi - 1];
     int dx = vx[xi] - vx[xi - 1];
     res += dy * dx;
-    //cout << yi << " " << xi << "\n";
-    //cout << vy[yi] << " " << vy[yi - 1] << " " << vx[xi] << " " << vx[xi - 1] << "\n";
 }
 
 void sol() {
-    //cout << vx.size() << " " << vy.size() << "\n";
     for (int y = 0; y < vy.size(); y++) { // y 이상 = 무효
         int a = 0, b = 0;
         for (int x = 1; x < vx.size(); x++) { // x 이상 = 무효
             for (auto p : vp[x - 1]) {
                 if (p.fi < y) {
-                    //cout << p.fi << " " << y << " y\n";
                     a += arr[p.se].fi;
                     b += arr[p.se].se;
                 }
             }
-            //cout << a << " " << b << "ab \n";
             if (a > b) count(y, x);
         }
-
     }
 }
 
