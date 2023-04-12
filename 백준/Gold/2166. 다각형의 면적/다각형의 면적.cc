98cd36e123bf1e1c992c1ce739c24fda8a5cd335
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-04-13 01:22:56 +0900

    [Gold V] Title: 다각형의 면적, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double long double
+#define fi first
+#define se second
+#define all(a) (a).begin(), (a).end()
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using namespace std;
+using pii = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+
+struct Point {
+    int x, y;
+};
+Point operator + (Point a, Point b) { return { a.x + b.x, a.y + b.y }; } 
+Point operator - (Point a, Point b) { return { a.x - b.x, a.y - b.y }; }
+ll operator * (Point a, Point b) { return { (ll) a.x * b.x + (ll) a.y * b.y }; } // dot product
+ll operator ^ (Point a, Point b) { return { (ll) a.x * b.y - (ll) a.y * b.x }; } // cross product
+
+const int MAX = 10010;
+int n, res = 0;
+Point cord[MAX];
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> cord[i].x >> cord[i].y;
+    
+    for (int i = 2; i < n; i++) res += ((cord[i] - cord[0]) ^ (cord[i - 1] - cord[0]));
+    res = abs(res);
+    cout << fixed; cout.precision(1);
+    cout << (double) round(res * 5) / 10;
+}
