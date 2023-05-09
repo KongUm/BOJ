4923d98b55924bffb2ddcc12658ada4c25127d8c
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-05-10 01:40:09 +0900

    [Platinum V] Title: 볼록 껍질, Time: 36 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
@@ -14,30 +14,15 @@ Point operator - (Point a, Point b) { return { a.x - b.x, a.y - b.y }; }
 ll operator * (Point a, Point b) { return { (ll) a.x * b.x + (ll) a.y * b.y };}
 ll operator ^ (Point a, Point b) { return { (ll) a.x * b.y - (ll) a.y * b.x }; }
 
-int getDist(Point a) { return (a.x * a.x + a.y * a.y); }
-
-int n; 
-vector<Point> crd, hull;
+int n; Point lp = { INF, INF };
+vector<Point> crd;
+stack<Point> stack1;
 
 bool cmp(Point a, Point b) {
-    Point pa = a - crd[0], pb = b - crd[0];
+    Point pa = a - lp, pb = b - lp;
     int ret = pa ^ pb;
-    if (ret == 0) return getDist(pa) < getDist(pb);
+    if (ret == 0) return (abs(pa.x) + abs(pa.y)) < (abs(pb.x) + abs(pb.y));
     else return ret < 0;
-}
-
-void grahamScan() {
-    sort(crd.begin() + 1, crd.end(), cmp);
-
-    int idx = 0;
-    while (idx < n) {
-        while (hull.size() >= 2) {
-            auto p2 = hull.back(); hull.pop_back(); 
-            auto p1 = hull.back();
-            if (((p1 - crd[idx]) ^ (p2 - crd[idx])) < 0) { hull.push_back(p2); break; }
-        }
-        hull.push_back(crd[idx++]);
-    }
 }
 
 signed main() {
@@ -46,8 +31,18 @@ signed main() {
     cin >> n;
     for (int i = 0; i < n; i++) { 
         cin >> a >> b; crd.push_back({ a, b });
-        if (b < crd[0].y || (b == crd[0].y && a < crd[0].x)) swap(crd[0], crd[i]); 
+        if (b < lp.y || (lp.y == b && a < lp.x)) lp = { a, b }; 
     }
-    grahamScan();
-    cout << hull.size();
+    sort(all(crd), cmp);
+
+    int idx = 0;
+    while (idx < n) {
+        while (stack1.size() >= 2) {
+            auto p2 = stack1.top(); stack1.pop(); 
+            auto p1 = stack1.top();
+            if (((p1 - crd[idx]) ^ (p2 - crd[idx])) < 0) { stack1.push(p2); break; }
+        }
+        stack1.push(crd[idx++]);
+    }
+    cout << stack1.size();
 }
