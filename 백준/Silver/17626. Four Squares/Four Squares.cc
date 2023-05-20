65f5b3d8058e9f818607b84775d34a02828ef70a
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-05-21 02:26:45 +0900

    [Silver III] Title: Four Squares, Time: 92 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -1,14 +1,9 @@
 #include <bits/stdc++.h>
-#define int long long
-
 using namespace std;
-using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
 const int MAX = 50010;
 int n, dp[MAX];
 
 signed main() {
-    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     for (int i = 1; i < MAX; i++) dp[i] = (int) 1e9;
     cin >> n;
     for (int i = 0; i < 4; i++) {
