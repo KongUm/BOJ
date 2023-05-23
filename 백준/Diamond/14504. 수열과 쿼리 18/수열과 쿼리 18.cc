65db0273b8513745dfb1a8fefb7c1c60440697d9
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-05-24 02:22:19 +0900

    [Diamond V] Title: 수열과 쿼리 18, Time: 1548 ms, Memory: 4160 KB -BaekjoonHub

---
+++ 백준/Diamond/14504. 수열과 쿼리 18/수열과 쿼리 18.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double long double
+#define fi first
+#define se second
+#define all(a) (a).begin(), (a).end()
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using namespace std;
+using pii = pair<int, int>;
+using ti3 = tuple<int, int, int>;
+const int MAX = 100010;
+int n, m, sq, arr[MAX];
+vector<int> bucket[1010];
+
+void init() {
+    for (int i = 1; i < n + 1; i++) {
+        bucket[i / sq].push_back(arr[i]);
+    }
+    for (int i = 0; i < 1010; i++) {
+        if (bucket[i].size() > 0) sort(all(bucket[i]));
+    }
+}
+
+void update(int idx, int val) {
+    bool flag = true;
+    vector<int> tmp;
+    for (int i : bucket[idx / sq]) {
+        if (i != arr[idx] || !flag) tmp.push_back(i);
+        if (i == arr[idx] && flag) flag = false;
+    }
+    bucket[idx / sq].clear();
+
+    int i = 0;
+    while (i < tmp.size() && tmp[i] < val) bucket[idx / sq].push_back(tmp[i++]);
+    bucket[idx / sq].push_back(val);
+    while (i < tmp.size()) bucket[idx / sq].push_back(tmp[i++]);
+    arr[idx] = val;
+}   
+
+int query(int l, int r, int k) {
+    int res = 0;
+    while (l % sq != 0 && l <= r) {
+        if (arr[l] > k) res++;
+        l++;
+    }
+    while ((r + 1) % sq != 0 && l <= r) {
+        if (arr[r] > k) res++;
+        r--;
+    }
+
+    while (l <= r) {
+        int a = bucket[l / sq].end() - upper_bound(all(bucket[l / sq]), k);
+        res += a;
+        l += sq;
+    }
+    return res;
+}
+
+
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cin >> n; sq = sqrt(n);
+    for (int i = 1; i < n + 1; i++) cin >> arr[i];
+    init();
+    cin >> m;
+    int q, a, b, c;
+    for (int i = 0; i < m; i++) {
+        cin >> q >> a >> b;
+        if (q == 1) {
+            cin >> c;
+            cout << query(a, b, c) << "\n";
+        }
+        else update(a, b);
+    }  
+}
