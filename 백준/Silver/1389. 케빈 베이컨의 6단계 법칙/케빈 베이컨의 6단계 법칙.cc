3296ea5d5d2058ae55b59141766f2dfb255d134a
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-05-26 00:24:27 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 2116 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -1,15 +1,9 @@
 #include <bits/stdc++.h>
 #define int long long
-#define ll long long
-#define double long double
 #define fi first
 #define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-
 using namespace std;
 using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
 const int MAX = 110, INF = (int) 1e6;
 int n, m, dp[MAX][MAX];
 
