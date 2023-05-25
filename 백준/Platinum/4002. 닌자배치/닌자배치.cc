4ab7459b9d9d8855007fccceb70b4a028c3ad7bd
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-05-26 00:05:16 +0900

    [Platinum I] Title: 닌자배치, Time: 84 ms, Memory: 21844 KB -BaekjoonHub

--- 백준/Platinum/4002. 닌자배치/닌자배치.cc
+++ 백준/Platinum/4002. 닌자배치/닌자배치.cc
@@ -1,22 +1,13 @@
 #include <bits/stdc++.h>
 #define int long long
-#define ll long long
-#define double long double
-#define fi first
-#define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-
 using namespace std;
-using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
 const int MAX = 100010;
 int n, m, res = 0, par[MAX], cost[MAX], v[MAX], id[MAX], sum[MAX]; 
 priority_queue<int> pq[MAX];
 
 void merge(int a, int b) {
     if (pq[id[a]].size() < pq[id[b]].size()) swap(a, b);
-    
+
     while (pq[id[b]].size()) {
         int tmp = pq[id[b]].top(); pq[id[b]].pop();
         pq[id[a]].push(tmp), sum[id[a]] += tmp, sum[id[b]] -= tmp;
