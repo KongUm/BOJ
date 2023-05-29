111d44423179a91a2fa0bfefbcbc614adbebd1e6
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-05-29 23:10:49 +0900

    [Platinum I] Title: 트리, Time: 480 ms, Memory: 41788 KB -BaekjoonHub

--- 백준/Platinum/13309. 트리/트리.cc
+++ 백준/Platinum/13309. 트리/트리.cc
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
-#define int long long
-#define ll long long
-#define double long double
 #define fi first
 #define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
 
 using namespace std;
 using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
 const int MAX = 200010;
 int n, m, sz[MAX], dep[MAX], top[MAX], in[MAX], out[MAX], par[MAX], visited[MAX];
 vector<int> chd[MAX], graph[MAX];
 
-struct Seg { // Lazy Segment Tree
-    int segTree[MAX * 4], lazy[MAX * 4];
-    // dfs ordering 기준 + 각 정점의 부모와 연결하는 간선의 가중치를 의미
+struct Seg {
+    int segTree[MAX * 4];
 
     void update(int st, int ed, int nd, int idx, int val) {
         if (idx < st || ed < idx) return;
@@ -68,19 +61,17 @@ void dfs2(int cur) {
 }
 
 void update(int a) {
-    seg.update(1, MAX, 1, in[a], 1);
+seg.update(1, MAX, 1, in[a], 1);
 }
 
 int query(int a, int b) {
     int ret = 0;
-    while (top[a] != top[b]) { // a와 b가 같은 체인이 될때까지 반복
-        if (dep[top[a]] < dep[top[b]]) swap(a, b); // dep[top[a]] > dep[top[b]]가 되도록 바꿔줌
+    while (top[a] != top[b]) { 
+        if (dep[top[a]] < dep[top[b]]) swap(a, b); 
         ret = max(ret, seg.query(1, MAX, 1, in[top[a]], in[a]));
-        // 정점 a가 속해있는 체인의 top부터 a까지 쿼리 처리
-        a = par[top[a]]; // a가 속해있는 체인의 top의 부모로 올려줌 (Light-Edge를 타고 감) 
+        a = par[top[a]];
     }
     if (dep[a] > dep[b]) swap(a, b);
-    // dep[a] <= dep[b]가 되도록 변경 (즉 a노드가 더 상위 노드가 되도록)
     ret = max(ret, seg.query(1, MAX, 1, in[a] + 1, in[b]));
     return ret;
 }
