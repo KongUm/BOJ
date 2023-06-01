4aeeca8266ca731e472fccbed1045680f6cdb813
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-06-02 01:41:19 +0900

    [Gold IV] Title: 적의 적, Time: 236 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Gold/12893. 적의 적/적의 적.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define double long double
+#define fi first
+#define se second
+#define all(a) (a).begin(), (a).end()
+#define debug(x) cout << (#x) << ": " << (x) << '\n'
+
+using namespace std;
+const int MAX = 2010;
+int n, m, vis[MAX], flag;
+vector<int> g[MAX];
+
+void dfs(int cur, int c) {
+    vis[cur] = c;
+    for (int nxt : g[cur]) {
+        if (vis[nxt] == 0) dfs(nxt, ((c - 1) ^ 1) + 1);
+        else if (vis[cur] == vis[nxt]) flag = true;
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    cin >> n >> m;
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    for (int i = 1; i < n + 1; i++) {
+        if (!vis[i]) vis[i] = 1, dfs(i, 1);
+    }
+    cout << (1 ^ flag) << "\n";
+}
