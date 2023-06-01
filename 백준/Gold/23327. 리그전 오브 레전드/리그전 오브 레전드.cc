37d7037e31ade0a759f91bcde99c6b9fca21f614
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-06-02 00:47:16 +0900

    [Gold III] Title: 리그전 오브 레전드, Time: 84 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Gold/23327. 리그전 오브 레전드/리그전 오브 레전드.cc
+++ 백준/Gold/23327. 리그전 오브 레전드/리그전 오브 레전드.cc
@@ -1,27 +1,18 @@
 #include <bits/stdc++.h>
 #define int long long
-#define ll long long
-#define double long double
-#define fi first
-#define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-
 using namespace std;
 using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
 const int MAX = 100010;
-int n, q, arr[MAX], p[MAX], b[MAX], p2[MAX];
-
+int n, q, arr[MAX], p[MAX], p2[MAX];
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     cin >> n >> q;
     for (int i = 1; i < n + 1; i++) cin >> arr[i];
-    for (int i = 1; i < n + 1; i++) p[i] = p[i - 1] + arr[i];
-    for (int i = 1; i < n + 1; i++) b[i] = arr[i] * arr[i];
-    for (int i = 1; i < n + 1; i++) p2[i] = p2[i - 1] + b[i];
-    
+    for (int i = 1; i < n + 1; i++) {
+        p[i] = p[i - 1] + arr[i];
+        p2[i] = p2[i - 1] + arr[i] * arr[i];
+    }
 
     int a, b;
     for (int i = 0; i < q; i++) {
