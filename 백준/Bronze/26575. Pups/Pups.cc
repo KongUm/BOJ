18a06c8ccfaa299a6a3e5229a65a2c0afdc62538
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-06-04 17:56:39 +0900

    [Bronze V] Title: Pups, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/26575. Pups/Pups.cc
+++ 백준/Bronze/26575. Pups/Pups.cc
@@ -1,18 +1,7 @@
 #include <bits/stdc++.h>
-#define int long long
-#define ll long long
-#define fi first
-#define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-#define YES cout << "YES" << "\n"
-#define NO cout << "NO" << "\n"
-
 using namespace std;
-using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
-const int MAX = 100010;
 double a, b, c;
+
 void sol() {
     cin >> a >> b >> c;
     cout << "$";
