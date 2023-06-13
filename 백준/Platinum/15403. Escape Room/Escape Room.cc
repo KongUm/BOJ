2552ad3a702f4fa75f885a30231bcb527c0c277f
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-06-13 23:56:42 +0900

    [Platinum V] Title: Escape Room, Time: 24 ms, Memory: 4080 KB -BaekjoonHub

--- 백준/Platinum/15403. Escape Room/Escape Room.cc
+++ 백준/Platinum/15403. Escape Room/Escape Room.cc
@@ -1,28 +1,17 @@
 #include <bits/stdc++.h>
-#define int long long
-#define ll long long
-#define fi first
-#define se second
 #define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-
 using namespace std;
 using pii = pair<int, int>;
-const int MAX = 100010;
-int n, arr[MAX];
+int n, a, arr[100010];
 vector<pii> v;
 
 signed main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int a;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a;
-        v.push_back({ a, i });
-    }
+    for (int i = 0; i < n; i++) cin >> a, v.push_back({ a, i });
     sort(all(v));
     
     int num = n;
-    for (auto p : v) arr[p.se] = num--;
+    for (auto p : v) arr[p.second] = num--;
     for (int i = 0; i < n; i++) cout << arr[i] << " ";
 }
