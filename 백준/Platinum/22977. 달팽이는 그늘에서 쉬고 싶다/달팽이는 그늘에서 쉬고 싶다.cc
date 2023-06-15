2465210a4c7d5660bafb0ee22d21d779093c4af1
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-06-15 23:31:14 +0900

    [Platinum IV] Title: 달팽이는 그늘에서 쉬고 싶다, Time: 56 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Platinum/22977. 달팽이는 그늘에서 쉬고 싶다/달팽이는 그늘에서 쉬고 싶다.cc
+++ 백준/Platinum/22977. 달팽이는 그늘에서 쉬고 싶다/달팽이는 그늘에서 쉬고 싶다.cc
@@ -1,16 +1,9 @@
 #include <bits/stdc++.h>
 #define int long long
-#define ll long long
-#define double long double
 #define fi first
 #define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-
 using namespace std;
 using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
-const int MAX = 100010;
 int n, ans;
 vector<pii> v;
 
