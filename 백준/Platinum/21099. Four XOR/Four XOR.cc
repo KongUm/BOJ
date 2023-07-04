0ea246d8a9bfff2f7363fe7e33841c138e5911fe
JuHyeon- <76547054+KongUm@users.noreply.github.com>
2023-07-04 23:29:16 +0900

    [Platinum V] Title: Four XOR, Time: 8 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Platinum/21099. Four XOR/Four XOR.cc
+++ 백준/Platinum/21099. Four XOR/Four XOR.cc
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
-#define int long long
-#define ll long long
-#define double long double
-#define fi first
-#define se second
-#define all(a) (a).begin(), (a).end()
-#define debug(x) cout << (#x) << ": " << (x) << '\n'
-
 using namespace std;
-using pii = pair<int, int>;
-using ti3 = tuple<int, int, int>;
-const int MAX = 100010;
+const int MAX = 200010;
 int n, arr[MAX], cnt[MAX], chk[MAX], flag;
 
 signed main() {
@@ -19,14 +9,14 @@ signed main() {
     for (int i = 0; i < n; i++) cin >> arr[i];
     for (int i = 0; i < n; i++) if(++cnt[arr[i]] >= 4) flag = 1;
 
-    if (n > 10000 || flag) { cout << "Yes"; return 0; }
+    if (n >= 513 || flag) { cout << "Yes"; return 0; }
 
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) chk[arr[i] ^ arr[j]]++;
     }
 
     for (int i = 0; i < MAX; i++) if (chk[i] >= 2) flag = 1;
-    
+
     if (flag) cout << "Yes";
     else cout << "No";
 }
